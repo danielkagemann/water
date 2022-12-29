223247c0673b87c3fb93c7fa51deb5c644dbe16f
danielkagemann <info@danielkagemann.name>
2022-12-29 17:45:30 +0100

    Initial Commit

---
+++ water/waterApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterApp.swift
+//  water
+//
+//  Created by Daniel Kagemann on 29.12.22.
+//
+
+import SwiftUI
+
+@main
+struct waterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
