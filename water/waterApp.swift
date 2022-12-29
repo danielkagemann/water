ca62af4b3175219cdfa81831fba28cf95b227ef6
danielkagemann <info@danielkagemann.name>
2022-12-29 18:11:47 +0100

    initial version using realm

--- water/waterApp.swift
+++ water/waterApp.swift
@@ -1,17 +1,22 @@
 //
-//  waterApp.swift
-//  water
+//  wasserApp.swift
+//  wasser
 //
-//  Created by Daniel Kagemann on 29.12.22.
+//  Created by Daniel Kagemann on 28.12.22.
 //
 
 import SwiftUI
 
 @main
-struct waterApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+struct waterApp: SwiftUI.App {
+
+   var body: some Scene {
+      WindowGroup {
+         SplashScreenView()
+            .onAppear {
+               print (FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.path)
+               UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+            }
+      }
+   }
 }
